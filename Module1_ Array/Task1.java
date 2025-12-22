4d4a10004968e373877ff9f7db9a77dde19e770a
Varshitha3-wq <241801120013@cutmap.ac.in>
2025-12-22 16:22:50 +0530

    Update Task1.java

--- Module1_ Array/Task1.java
+++ Module1_ Array/Task1.java
@@ -1,25 +1,35 @@
-import java.util.*;
-public class Task1 {
+class Main {
     public static void main(String[] args) {
-       // Static D & I  
-        int[] n={1,2,3,4,5};
-        System.out.println(n[0]);
-        System.out.println(n.length);
-
-         for(int i=0;i<n.length;i++){
-            System.out.print(n[i]+" ");
-         }
-
-         // key word
-
-         int[] a= new int[] {1,2,3,4,5};
-            for(int i=0;i<a.length;i++){
-                System.out.print(a[i]+" ");
-            }
-             int[] b= new int[10];
-
-             b[0]=10;
-             b[1]=20;
-             for(int i=0;i<b.length;i++){
-                System.out.print(n2[i]+" ");
-             }}
+        
+        
+        // static D & I
+        int[] num = {1,2,3};
+        for(int i=0;i<num.length;i++){
+            System.out.println(num[i]);
+        }
+        
+        String[] names = {"sai","raju","sony"};
+        for(int i=0;i<names.length;i++){
+            System.out.println(names[i]);
+        }
+        
+        // new
+         int[] num1 = new int[] {4,5,6};
+        for(int i=0;i<num1.length;i++){
+            System.out.println(num1[i]);
+        }
+        
+        String[] names1 = new String[] {"lakshmi","kavya","phani"};
+        for(int i=0;i<names1.length;i++){
+            System.out.println(names1[i]);
+        }
+        
+        int[] da = new int[10];
+        da[0]=100;
+        da[da.length-1]=900;
+        for(int i=0;i<da.length;i++){
+            System.out.println(da[i]);
+        }
+        
+ }
+}
